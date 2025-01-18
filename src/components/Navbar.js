197ab8f9bdea9b1bd45f53b420c3cea3506e8e51
aleksanderbialka <aleksander.bialka@icloud.com>
2025-01-18 21:08:08 +0100

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import weatherLogo from '../assets/weather-logo.png';
+
+const Navbar = () => {
+    return (
+        <AppBar
+            position="static"
+            sx={{
+                backgroundColor: 'rgba(0, 0, 0, 0.1)',
+                boxShadow: 'none',
+                borderRadius: '20px',
+                backdropFilter: 'blur(10px)',
+            }}
+        >
+            <Toolbar>
+                <img
+                    src={weatherLogo}
+                    alt="Weather Logo"
+                    style={{ height: '40px', marginRight: '16px' }}
+                />
+                <Typography variant="h6"  sx={{ flexGrow:1, ml: 1, color: 'black' }}>
+                    PewnaPogoda.pl
+                </Typography>
+                <Tabs
+                      sx={{
+                        color: 'black',
+                    }}
+                >
+                    <Tab label="Pogoda 7 dni" />
+                    <Tab label="Pogoda 14 dni" />
+                </Tabs>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+
+import Navbar from "../components/Navbar";
+
+function Dashboard() {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  );
+}
+
+export default Dashboard;
